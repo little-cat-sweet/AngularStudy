1d745c4aaf4be409c18a6602b12638b2194ffe27
Sky_Blue <2116400472@qq.com>
2023-02-08 22:02:15 +0800

    create a serve by ng g s (serve name)

---
+++ src/app/serves/title.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  //root表示注入到app.module.ts内， null, 表示不设定区域，还可以是某个模块的名字(一般都是懒加载模式)。
+  providedIn: 'root'
+})
+export class TitleService {
+
+  constructor() { }
+}
